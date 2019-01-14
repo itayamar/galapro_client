684b641f3044a2056760b87dd09c5a440c7ab2d6
itayamar@gmail.com <th,h3575>
2019-01-14 11:12:01 +0200

    Part 2 (Incomplete ) - getting an error, maybe its only for my env ( posted a stackOverflow question)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+    import { BrowserModule } from '@angular/platform-browser';
+    import { NgModule } from '@angular/core';
+    import { FormsModule } from '@angular/forms';
+    import { HttpModule } from '@angular/http';
 
-import { AppComponent } from './app.component';
-import { ActionsComponent } from './actions/actions.component';
-import { RouterModule, Routes } from '@angular/router';
-import { DataComponent } from './data/data.component';
+    import { AppComponent } from './app.component';
+    import { ActionsComponent } from './actions/actions.component';
+    import { RouterModule, Routes } from '@angular/router';
+    import { DataComponent } from './data/data.component';
+    import {UsersService} from './users.service';
 
 
-const appRoutes: Routes = [
-  { path: '', redirectTo: '/actions', pathMatch: 'full' },
-  { path: 'actions', component: ActionsComponent },
-  { path: 'data', component: DataComponent, },
-];
+    const appRoutes: Routes = [
+      { path: '', redirectTo: '/actions', pathMatch: 'full' },
+      { path: 'actions', component: ActionsComponent },
+      { path: 'data', component: DataComponent, },
+    ];
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    ActionsComponent,
-    DataComponent
-  ],
-  imports: [
-    BrowserModule,
-    RouterModule.forRoot(appRoutes),
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
+    @NgModule({
+      declarations: [
+        AppComponent,
+        ActionsComponent,
+        DataComponent
+      ],
+
+      imports: [
+        BrowserModule,
+        RouterModule.forRoot(appRoutes),
+        FormsModule,
+        HttpModule
+      ],
+      providers: [UsersService],
+      bootstrap: [AppComponent]
+    })
+    export class AppModule { }
--- src/app/users.service.ts
+++ src/app/users.service.ts
@@ -1,33 +1,51 @@
-import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import {Injectable} from '@angular/core';
+import {Observable, of} from 'rxjs';
+import {Http, Response} from '@angular/http';
+import 'rxjs/add/operator/map';
 
 import {User} from './user';
+
 const usersLocalStorageKey = 'users';
 
 @Injectable()
 export class UsersService {
   private static users: User[];
-  constructor() { }
+
+  constructor(private _http: Http) {}
+
   public static getUsers(): Observable<User[]> {
+    //step 1
     this.users = JSON.parse(localStorage.getItem(usersLocalStorageKey)) || [];
     return of(this.users);
+
+    //step 2
+    // todo: Fix error Property '_http' does not exist on type 'typeof UsersService'
+    //return this._http.get('http://localhost:3000/users')
+    //  .map((res: Response) => <User[]>res.json());
   }
-  private static createUser (username: string, userValue: number, userMessage: string): User {
+
+  private static createUser(username: string, userValue: number, userMessage: string): User {
     return {
       name: username,
       value: userValue,
       message: userMessage
     };
   }
+
   public static addUser(userName: string, userValue: number, userMessage: string): void {
     let userToAdd = this.createUser(userName, userValue, userMessage);
     this.getUsers().subscribe((users: User[]) => {
       this.users = users;
       this.users.push(userToAdd);
+
+      //step 1
       localStorage.setItem(usersLocalStorageKey, JSON.stringify(this.users));
 
-      console.log('USERS', this.users);
+      //step 2
+      // todo: Fix error Property '_http' does not exist on type 'typeof UsersService'
+      //this._http.post('http://localhost:3000/users', userToAdd);
     });
   }
+
 }
 
